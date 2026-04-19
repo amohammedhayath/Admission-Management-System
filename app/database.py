9da4652065c833b4a49d084d36b29fbcadb4a625
Shaik Nayab Mohammed Hayath <114560432+amohammedhayath@users.noreply.github.com>
2026-04-19 12:57:38 +0530

    fix: remove dotenv for render env

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
+# load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 # ✅ Don't crash — fallback for safety
